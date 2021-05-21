9d0b893f7c97daed39b92f9fa05854e39bf779de
gindis <gindis.wx@gmail.com>
2021-05-21 10:46:12 +0800

    feat: init

---
+++ test/data.js
@@ -0,0 +1,515 @@
+module.exports = {
+  "artboardImg": "https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/b58c8c6069bb11ebb0a345edf149744d.png",
+  "rect": {
+    "x": 0,
+    "y": 0,
+    "width": 375,
+    "height": 168
+  },
+  "pluginVersion": "2.4.0",
+  "componentName": "Block",
+  "name": "锁库记录",
+  "reference": "sketch",
+  "restore_id": "a36b2b91-8f99-4aa2-bc93-621b4e1921de",
+  "props": {
+    "style": {
+      "width": "375px",
+      "height": "168px"
+    },
+    "className": "mod"
+  },
+  "id": "bcada6f0-69bb-11eb-89a7-b1e797f8660c",
+  "children": [{
+    "componentName": "Div",
+    "id": "bcadce0a-69bb-11eb-89a7-b1e797f8660c",
+    "props": {
+      "style": {
+        "display": "flex",
+        "alignItems": "flex-start",
+        "flexDirection": "column",
+        "backgroundColor": "#FFFFFF",
+        "width": "375px",
+        "height": "168px"
+      },
+      "className": "box"
+    },
+    "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 375,
+      "height": 168
+    },
+    "selfId": "C22A4D1E-CE8D-4348-B113-5D8E4B0B21A2",
+    "children": [{
+      "componentName": "Text",
+      "id": "bcadce09-69bb-11eb-89a7-b1e797f8660c",
+      "props": {
+        "style": {
+          "position": "relative",
+          "marginTop": "12px",
+          "marginLeft": "15px",
+          "maxWidth": "360px",
+          "height": "18px",
+          "overflow": "hidden",
+          "textOverflow": "ellipsis",
+          "lineHeight": "18px",
+          "whiteSpace": "nowrap",
+          "color": "#181818",
+          "fontSize": "13px",
+          "fontWeight": 400
+        },
+        "text": "锁库件数:366件/3天",
+        "lines": 1,
+        "className": "info"
+      },
+      "rect": {
+        "x": 15,
+        "y": 12,
+        "width": 120,
+        "height": 18
+      },
+      "selfId": "DDD4D887-6D56-44D4-94E7-914D6FE15DC00"
+    }, {
+      "componentName": "Div",
+      "id": "ded86324-2649-47eb-a847-dba8754e2ac2",
+      "props": {
+        "style": {
+          "display": "flex",
+          "position": "relative",
+          "alignItems": "flex-end",
+          "alignSelf": "center",
+          "flexDirection": "row",
+          "height": "17px"
+        },
+        "className": "bd"
+      },
+      "rect": {
+        "x": 16,
+        "y": 30,
+        "width": 343,
+        "height": 17
+      },
+      "children": [{
+        "componentName": "Text",
+        "id": "bcadce08-69bb-11eb-89a7-b1e797f8660c",
+        "props": {
+          "style": {
+            "marginRight": "154px",
+            "marginBottom": "2px",
+            "lineHeight": "10px",
+            "whiteSpace": "pre",
+            "color": "#acacac",
+            "fontSize": "10px",
+            "fontWeight": 400
+          },
+          "text": "创建时间：2020-01-23 23:30:56",
+          "lines": 1,
+          "className": "word"
+        },
+        "rect": {
+          "x": 16,
+          "y": 33,
+          "width": 153,
+          "height": 14
+        },
+        "selfId": "EFCF5272-0F1D-4052-915A-0BA10CDCC1850"
+      }, {
+        "componentName": "Text",
+        "id": "bcadce03-69bb-11eb-89a7-b1e797f8660c",
+        "props": {
+          "style": {
+            "marginBottom": "2px",
+            "lineHeight": "12px",
+            "whiteSpace": "nowrap",
+            "color": "#fc9627",
+            "fontSize": "12px",
+            "fontWeight": 400
+          },
+          "text": "锁库中",
+          "lines": 1,
+          "className": "text"
+        },
+        "rect": {
+          "x": 323,
+          "y": 30,
+          "width": 36,
+          "height": 17
+        },
+        "selfId": "F1674E8C-E8FD-4518-A92E-A79164B3E0150"
+      }]
+    }, {
+      "componentName": "Div",
+      "id": "a148c07f-398b-4619-abdc-868e5de754c1",
+      "props": {
+        "style": {
+          "display": "flex",
+          "position": "relative",
+          "alignItems": "center",
+          "alignSelf": "center",
+          "flexDirection": "row",
+          "marginTop": "11px"
+        },
+        "className": "ft"
+      },
+      "rect": {
+        "x": 16,
+        "y": 58,
+        "width": 343,
+        "height": 99
+      },
+      "children": [{
+        "componentName": "Image",
+        "id": "bcada6f1-69bb-11eb-89a7-b1e797f8660c",
+        "props": {
+          "style": {
+            "marginTop": "-1px",
+            "marginRight": "8px",
+            "width": "98px",
+            "height": "98px",
+            "overflow": "hidden"
+          },
+          "src": "https://ai-sample.oss-cn-hangzhou.aliyuncs.com/test/b52df1a069bb11ebb1da87076ef63466.png",
+          "className": "pic"
+        },
+        "rect": {
+          "x": 16,
+          "y": 58,
+          "width": 98,
+          "height": 98
+        },
+        "selfId": "5032776C-6C9D-4552-B391-E8DDC351C164"
+      }, {
+        "componentName": "Div",
+        "id": "8361628d-e3c9-4ab3-98e9-61933ff4a530",
+        "props": {
+          "style": {
+            "display": "flex",
+            "alignItems": "flex-start",
+            "flexDirection": "column",
+            "marginTop": "1px"
+          },
+          "className": "group"
+        },
+        "rect": {
+          "x": 122,
+          "y": 59,
+          "width": 237,
+          "height": 98
+        },
+        "children": [{
+          "componentName": "Text",
+          "id": "bcada6f2-69bb-11eb-89a7-b1e797f8660c",
+          "props": {
+            "style": {
+              "WebkitBoxOrient": "vertical",
+              "display": "-webkit-inline-box",
+              "WebkitLineClamp": 2,
+              "position": "relative",
+              "width": "236px",
+              "height": "36px",
+              "overflow": "hidden",
+              "textOverflow": "ellipsis",
+              "lineHeight": "18px",
+              "letterSpacing": "0.20px",
+              "color": "#181818",
+              "fontSize": "14px",
+              "fontWeight": 400
+            },
+            "text": "HRMES时尚个性格纹休闲运动风西装日常出街款",
+            "lines": 2,
+            "className": "desc"
+          },
+          "rect": {
+            "x": 122,
+            "y": 59,
+            "width": 236,
+            "height": 36
+          },
+          "selfId": "5C8E9BF1-9D7C-4ED8-A725-61057800C2D40"
+        }, {
+          "componentName": "Text",
+          "id": "bcadce02-69bb-11eb-89a7-b1e797f8660c",
+          "props": {
+            "style": {
+              "position": "relative",
+              "marginTop": "3px",
+              "marginLeft": "2px",
+              "maxWidth": "223px",
+              "height": "14px",
+              "overflow": "hidden",
+              "textOverflow": "ellipsis",
+              "lineHeight": "14px",
+              "whiteSpace": "nowrap",
+              "color": "#acacac",
+              "fontSize": "10px",
+              "fontWeight": 400
+            },
+            "text": "锁库下单件数：210件",
+            "lines": 1,
+            "className": "info_1"
+          },
+          "rect": {
+            "x": 124,
+            "y": 98,
+            "width": 97,
+            "height": 14
+          },
+          "selfId": "1BAE875C-FD87-4C63-9F02-B409A4F9F3E20"
+        }, {
+          "componentName": "Div",
+          "id": "029efaab-d877-485c-a7c4-dc6b090404bd",
+          "props": {
+            "style": {
+              "display": "flex",
+              "position": "relative",
+              "alignItems": "flex-end",
+              "flexDirection": "row",
+              "marginTop": "22px"
+            },
+            "className": "container"
+          },
+          "rect": {
+            "x": 122,
+            "y": 134,
+            "width": 237,
+            "height": 23
+          },
+          "children": [{
+            "componentName": "Div",
+            "id": "0335eedd-8dcf-45a4-b44d-fcbb8252bf9b",
+            "props": {
+              "style": {
+                "display": "flex",
+                "alignItems": "center",
+                "flexDirection": "row",
+                "marginRight": "27px",
+                "height": "17px"
+              },
+              "className": "priceWrap"
+            },
+            "rect": {
+              "x": 122,
+              "y": 139,
+              "width": 50,
+              "height": 17
+            },
+            "children": [{
+              "componentName": "Text",
+              "id": "bcada6f3-69bb-11eb-89a7-b1e797f8660c",
+              "props": {
+                "style": {
+                  "marginTop": "1px",
+                  "marginRight": "-2px",
+                  "lineHeight": "12px",
+                  "whiteSpace": "nowrap",
+                  "color": "#fd2020",
+                  "fontSize": "12px",
+                  "fontWeight": 400
+                },
+                "text": "￥",
+                "lines": 1,
+                "className": "yuan"
+              },
+              "rect": {
+                "x": 122,
+                "y": 140,
+                "width": 12,
+                "height": 16
+              },
+              "selfId": "DF39DACF-E9B5-478A-B1BC-2D8D82CF7A580"
+            }, {
+              "componentName": "Text",
+              "id": "bcadce01-69bb-11eb-89a7-b1e797f8660c",
+              "props": {
+                "style": {
+                  "marginTop": "-1px",
+                  "marginRight": "1px",
+                  "lineHeight": "15px",
+                  "whiteSpace": "nowrap",
+                  "color": "#fd2020",
+                  "fontSize": "15px",
+                  "fontWeight": 400
+                },
+                "text": "99",
+                "lines": 1,
+                "className": "price"
+              },
+              "rect": {
+                "x": 132,
+                "y": 139,
+                "width": 18,
+                "height": 16
+              },
+              "selfId": "5A342968-ACD9-4FF3-B4F3-BAD4E2365D350"
+            }, {
+              "componentName": "Text",
+              "id": "bcadce00-69bb-11eb-89a7-b1e797f8660c",
+              "props": {
+                "style": {
+                  "marginTop": "-1px",
+                  "lineHeight": "14px",
+                  "whiteSpace": "nowrap",
+                  "color": "#fd2020",
+                  "fontSize": "14px",
+                  "fontWeight": 400
+                },
+                "text": ".00",
+                "lines": 1,
+                "className": "count"
+              },
+              "rect": {
+                "x": 151,
+                "y": 139,
+                "width": 21,
+                "height": 16
+              },
+              "selfId": "5A342968-ACD9-4FF3-B4F3-BAD4E2365D351"
+            }]
+          }, {
+            "componentName": "Div",
+            "id": "7a268b13-1fc7-4b50-830f-f6bb38eb494f",
+            "props": {
+              "style": {
+                "display": "flex",
+                "alignItems": "center",
+                "flexDirection": "row"
+              },
+              "className": "block"
+            },
+            "rect": {
+              "x": 199,
+              "y": 134,
+              "width": 160,
+              "height": 23
+            },
+            "children": [{
+              "componentName": "Div",
+              "id": "bcadce05-69bb-11eb-89a7-b1e797f8660c",
+              "props": {
+                "style": {
+                  "boxSizing": "border-box",
+                  "display": "flex",
+                  "alignItems": "center",
+                  "flexDirection": "row",
+                  "marginRight": "12px",
+                  "borderWidth": "1px",
+                  "borderStyle": "solid",
+                  "borderRadius": "11.85882352941178px",
+                  "borderColor": "#E2E2E2",
+                  "backgroundColor": "#FFFFFF",
+                  "paddingRight": "16px",
+                  "paddingLeft": "16px",
+                  "height": "23px"
+                },
+                "className": "tagWrap"
+              },
+              "rect": {
+                "x": 199,
+                "y": 134,
+                "width": 74,
+                "height": 23
+              },
+              "selfId": "EE8D1EB1-9FBF-4A60-9FBA-E396AD402079",
+              "children": [{
+                "componentName": "Text",
+                "id": "bcadce04-69bb-11eb-89a7-b1e797f8660c",
+                "props": {
+                  "style": {
+                    "marginTop": "0px",
+                    "lineHeight": "10px",
+                    "whiteSpace": "nowrap",
+                    "color": "#7b7b7b",
+                    "fontSize": "10px",
+                    "fontWeight": 400
+                  },
+                  "text": "锁库明细",
+                  "lines": 1,
+                  "className": "tag"
+                },
+                "rect": {
+                  "x": 216,
+                  "y": 137,
+                  "width": 40,
+                  "height": 18
+                },
+                "selfId": "2CC71D8F-8E73-48EE-B22C-4F7B41A053560"
+              }],
+              "smart": {
+                "layerProtocol": {
+                  "repeat": {
+                    "type": "repeat",
+                    "repeatId": "1612754061490418",
+                    "repeatIndex": "0"
+                  }
+                }
+              }
+            }, {
+              "componentName": "Div",
+              "id": "bcadce07-69bb-11eb-89a7-b1e797f8660c",
+              "props": {
+                "style": {
+                  "boxSizing": "border-box",
+                  "display": "flex",
+                  "alignItems": "center",
+                  "flexDirection": "row",
+                  "borderWidth": "1px",
+                  "borderStyle": "solid",
+                  "borderRadius": "11.85882352941178px",
+                  "borderColor": "#E2E2E2",
+                  "backgroundColor": "#FFFFFF",
+                  "paddingRight": "16px",
+                  "paddingLeft": "16px",
+                  "height": "23px"
+                },
+                "className": "tagWrap_1"
+              },
+              "rect": {
+                "x": 285,
+                "y": 134,
+                "width": 74,
+                "height": 23
+              },
+              "selfId": "21CCC873-CA7F-45C1-B65B-0DF2D22CB17F",
+              "children": [{
+                "componentName": "Text",
+                "id": "bcadce06-69bb-11eb-89a7-b1e797f8660c",
+                "props": {
+                  "style": {
+                    "marginTop": "0px",
+                    "lineHeight": "10px",
+                    "whiteSpace": "nowrap",
+                    "color": "#7b7b7b",
+                    "fontSize": "10px",
+                    "fontWeight": 400
+                  },
+                  "text": "取消锁库",
+                  "lines": 1,
+                  "className": "tag_1"
+                },
+                "rect": {
+                  "x": 302,
+                  "y": 137,
+                  "width": 40,
+                  "height": 18
+                },
+                "selfId": "5FAEE36D-934A-4B7C-9D39-6CFC432BBA370"
+              }],
+              "smart": {
+                "layerProtocol": {
+                  "repeat": {
+                    "type": "repeat",
+                    "repeatId": "1612754061490418",
+                    "repeatIndex": "1"
+                  }
+                }
+              }
+            }]
+          }]
+        }]
+      }]
+    }]
+  }],
+  "imgcook": {
+    "restore_id": "a36b2b91-8f99-4aa2-bc93-621b4e1921de"
+  }
+}
